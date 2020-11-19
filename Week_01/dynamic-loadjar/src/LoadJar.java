758acc03333871396c4267b25e495f735a2308ad
zhoutzzz <925920668@qq.com>
2020-11-19 15:43:03 +0800

    动态加载外部jar包

---
+++ Week_01/dynamic-loadjar/src/LoadJar.java
@@ -0,0 +1,19 @@
+import java.lang.reflect.Method;
+import java.net.URL;
+import java.net.URLClassLoader;
+
+public class LoadJar {
+
+    public static void main(String[] args) throws Exception {
+
+        URLClassLoader urlClassLoader = new URLClassLoader(new URL[]{new URL("file:hello/hello.xar")});
+        Class<?> demo = urlClassLoader.loadClass("Demo");
+        Object demoInstance = demo.newInstance();
+        Method dynamicLoadClass = demo.getMethod("dynamicLoadClass", String.class);
+        for (int i = 0; i < 5; i++) {
+            Object hello = dynamicLoadClass.invoke(demoInstance, "Hello");
+            System.out.println(hello);
+        }
+        System.gc();
+    }
+}
