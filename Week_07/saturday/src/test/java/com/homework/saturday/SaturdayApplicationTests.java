2b2d6de399bac20e913590c0ff608a2397711f35
zhoutzzz <925920668@qq.com>
2020-11-30 11:11:48 +0800

    saturday

---
+++ Week_07/saturday/src/test/java/com/homework/saturday/SaturdayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.homework.saturday;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SaturdayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
